03adac405d7b2ab2a90d7ce1b43a9c40aac3e053
celbarrah <c.elbarrah@webeuz.com>
2026-04-07 14:11:05 +0200

    modify pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,17 +24,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr" className={inter.variable}>
       <head>
-       {/* Google Tag Manager */}
+        {/* Google Tag Manager */}
         <Script
           id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MB3WSPX3');`,
-        }}
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KF46QTDD');`,
+          }}
         />
         {/* End Google Tag Manager */}
       </head>
@@ -42,14 +42,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
          {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MB3WSPX3"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KF46QTDD"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-
         {children}
       </body>
     </html>
